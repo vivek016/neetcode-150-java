3e6b27ae3adc4af2f68326311755400f437a17a1
Vivek <vivekbalaji16@gmail.com>
2026-09-07 15:51:01 +0530

    Add brute-force solution for Contains Duplicate

--- src/ArraysAndHashing/ContainsDuplicate.java
+++ src/ArraysAndHashing/ContainsDuplicate.java
@@ -1,5 +1,18 @@
 package ArraysAndHashing;
 
 public class ContainsDuplicate {
+    // Brute Force
+    // Time: O(n²)
+    // Space: O(1)
+    public static boolean hasDuplicateBruteForce(int[] nums) {
 
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
