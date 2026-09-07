0c08374fa9c4a78378ef84a96dc851c6b5e54c67
Vivek <vivekbalaji16@gmail.com>
2026-09-07 15:53:39 +0530

    Add optimized solution for Contains Duplicate

--- src/ArraysAndHashing/ContainsDuplicate.java
+++ src/ArraysAndHashing/ContainsDuplicate.java
@@ -1,5 +1,7 @@
 package ArraysAndHashing;
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
     // Brute Force
     // Time: O(n²)
@@ -13,6 +15,24 @@ public class ContainsDuplicate {
                 }
             }
         }
+        return false;
+    }
+
+    // Optimal
+    // Time: O(n) average
+    // Space: O(n)
+    public boolean hasDuplicate(int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+
+            seen.add(num);
+        }
+
         return false;
     }
 }
